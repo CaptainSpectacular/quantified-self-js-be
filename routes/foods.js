27b329cebc8cb933c7490b4ce7aeb2e2ab8ed4d6
pollockcl <pollock.clee@gmail.com>
2018-06-29 17:25:26 -0600

    added patch to foods/{id}. its kind of sloppy right now and needs a refactor

--- routes/foods.js
+++ routes/foods.js
@@ -32,13 +32,42 @@ router.post("/", function(req, res, next) {
     });
 });
 
+// PATCH foods
+router.patch("/:id", function(req, res, next) {
+    var id = req.url.split("/")[1];
+    var name = req.body.name;
+    var calories = req.body.calories;
+
+    if (name && calories) {
+        database.raw("UPDATE foods SET name = ?, calories = ? WHERE id = ?", [name, calories, id]).then((data) => {
+            res.send(data);
+        }).catch((error) => {
+            console.log(error);
+            next();
+        });
+    } else if (name) {
+        database.raw("UPDATE foods SET name = ? WHERE id = ?", [name, id]).then((data) => {
+            res.send(data);
+        }).catch((error) => {
+            console.log(error);
+            next();
+        });
+    } else {
+        database.raw("UPDATE foods SET calories = ? WHERE id = ?", [calories, id]).then((data) => {
+            res.send(data);
+        }).catch((error) => {
+            console.log(error);
+            next();
+        });
+    };
+});
+
 // DELETE foods
 router.delete("/:id", function(req, res, next) {
     var id = req.url.split("/")[1];
     database.raw("DELETE FROM foods WHERE id = ?", id).then((data) => {
         res.send(data);
     }).catch((error) => {
-        console.log(error);
         next();
     });
 });
