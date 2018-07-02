599893f353331afcb840fe6986becb581d68e389
pollockcl <pollock.clee@gmail.com>
2018-07-01 19:18:17 -0600

    fixed another typo

--- db/seeds/dev/meals.js
+++ db/seeds/dev/meals.js
@@ -5,7 +5,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('meals').insert([
         {id: 1, name: 'Breakfast'},
-        {id: 2, name: 'Snacks'},
+        {id: 2, name: 'Snack'},
         {id: 3, name: 'Lunch'},
         {id: 4, name: 'Dinner'}
       ]);
