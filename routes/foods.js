8e89afa084e9b92905b3e55f83c07047003188ba
pollockcl <pollock.clee@gmail.com>
2018-07-02 15:48:18 -0600

    added put route to same controller

--- routes/foods.js
+++ routes/foods.js
@@ -5,6 +5,7 @@ const foodsController = require("../controllers/foodsController");
 router.get("/", foodsController.index);
 router.get("/:id", foodsController.show); 
 router.post("/", foodsController.create); 
+router.put("/:id", foodsController.update); 
 router.patch("/:id", foodsController.update); 
 router.delete("/:id", foodsController.destroy) 
 
